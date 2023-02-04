a0206359791bd1efb8510799cf925d1e2020dcd4
Johan BEAUCE <jbeauce@pictet-technologies.com>
2023-02-04 16:34:56 +0100

    Add first test, when you add "", should return 0

---
+++ src/main/java/com/beauce/StringCalculator.java
@@ -0,0 +1,7 @@
+package com.beauce;
+
+public class StringCalculator {
+    public int add(String numbers) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/beauce/StringCalculatorTest.java
@@ -0,0 +1,23 @@
+package com.beauce;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Given StringCalculator class")
+class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    void when_empty_should_return_0() {
+        assertThat(stringCalculator.add("")).isZero();
+    }
+}
